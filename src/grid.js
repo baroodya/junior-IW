979d90bed74421b481368c34fe4a1a67d2e1b330
Alex Baroody <abaroody@princeton.edu>
2022-02-18 16:32:14 -0500

    clean up grid.js

--- src/grid.js
+++ src/grid.js
@@ -1,34 +1,31 @@
+// A wrapper for the visual component
 const Grid = (props) => {
-   return (
-      <div 
-         id={props.id} 
-         className="Grid"
-         children={props.children}
-      />
-   );
-}
+  return <div id={props.id} className="Grid" children={props.children} />;
+};
 
+// A row of cells
 const Row = (props) => {
-   return (
-      <div
-         id={props.id} 
-         index={props.index}
-         className="Row"
-         children={props.children} 
-      />
-   );
-}
+  return (
+    <div
+      id={props.id}
+      className="Row"
+      children={props.children}
+      index={props.index}
+    />
+  );
+};
 
+// 16 icons in a block
 const Cell = (props) => {
-   return (
-      <div 
-         id={props.id} 
-         index={props.index}
-         className="Cell"
-         children={props.children}
-         onClick={props.onClick} 
-      />
-   );
-}
+  return (
+    <div
+      id={props.id}
+      className="Cell"
+      children={props.children}
+      index={props.index}
+      onClick={props.onClick}
+    />
+  );
+};
 
-export {Grid, Row, Cell};
+export { Grid, Row, Cell };
