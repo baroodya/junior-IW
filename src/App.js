5eeba377d0838d9cb562e89bf0a3d8223e4b58bc
Alex Baroody <abaroody@princeton.edu>
2022-02-18 17:08:16 -0500

    cleanup App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { Grid, Row, Cell } from "./grid.js";
 import personIcon from "./person-icon-16.png";
+import { SlideContent } from "./SlideContent.js";
+
+/* Constants ******************************************************************/
 
 let WINDOW_HEIGHT_PIXELS = window.innerHeight;
 let NUM_ROWS = 40;
@@ -10,16 +13,21 @@ let CELL_HEIGHT = 4;
 let NUM_INFECTED_ROWS = NUM_ROWS / (CELL_HEIGHT * 2);
 
 function App() {
+  /* Functions ****************************************************************/
+
+  // Update on user scroll
   function onPageScroll() {
     let scrollHeight = document.documentElement.scrollTop;
 
-    let slide1Sep = (scrollHeight - WINDOW_HEIGHT_PIXELS) / 16;
-    let slide1Opacity = (scrollHeight / WINDOW_HEIGHT_PIXELS - 1) / 5;
+    // Update the arrows
+    setArrowVisibility(scrollHeight);
 
     switch (true) {
-      // Slide 0
+      // Slide 0 / Title Page
       case scrollHeight < WINDOW_HEIGHT_PIXELS: {
         let grid = document.getElementById("tutorial-visual");
+
+        // default settings for all changes
         for (const row of grid.childNodes) {
           row.style.transform = "translateY(0px)";
           row.style.background = "rgba(0,255,0,0)";
@@ -29,15 +37,22 @@ function App() {
 
       // Slide 1
       case scrollHeight <= 2 * WINDOW_HEIGHT_PIXELS: {
+        let sep = (scrollHeight - WINDOW_HEIGHT_PIXELS) / 16;
+        let opacity = (scrollHeight / WINDOW_HEIGHT_PIXELS - 1) / 5;
+
         let grid = document.getElementById("tutorial-visual");
+
+        // Change position and opacity linearly over transition
         for (const row of grid.childNodes) {
           let index = row.getAttribute("index");
+
+          // split based on NUM_INFECTED_ROWS
           if (index >= NUM_INFECTED_ROWS) {
-            row.style.transform = "translateY(" + slide1Sep + "px)";
-            row.style.background = "rgba(255,0,0," + slide1Opacity + ")";
+            row.style.transform = "translateY(" + sep + "px)";
+            row.style.background = "rgba(255,0,0," + opacity + ")";
           } else {
-            row.style.transform = "translateY(" + -slide1Sep + "px)";
-            row.style.background = "rgba(0,255,0," + slide1Opacity + ")";
+            row.style.transform = "translateY(" + -sep + "px)";
+            row.style.background = "rgba(0,255,0," + opacity + ")";
           }
         }
         break;
@@ -47,6 +62,7 @@ function App() {
     }
   }
 
+  // Scroll to previous page when user clicks up arrow; check the arrow opacity
   function onUpArrowClick() {
     let slideNum = Math.floor(
       -0.001 + document.documentElement.scrollTop / WINDOW_HEIGHT_PIXELS
@@ -54,9 +70,10 @@ function App() {
     let newHeight = slideNum * WINDOW_HEIGHT_PIXELS;
     window.scroll({ top: newHeight, behavior: "smooth" });
 
-    checkArrowOpacity(newHeight);
+    setArrowVisibility(newHeight);
   }
 
+  // Scroll to next page when user clicks down arrow; check the arrow opacity
   function onDownArrowClick() {
     let slideNum = Math.ceil(
       0.001 + document.documentElement.scrollTop / WINDOW_HEIGHT_PIXELS
@@ -64,61 +81,89 @@ function App() {
     let newHeight = slideNum * WINDOW_HEIGHT_PIXELS;
     window.scroll({ top: newHeight, behavior: "smooth" });
 
-    checkArrowOpacity(newHeight);
+    setArrowVisibility(newHeight);
   }
 
-  function checkArrowOpacity(scrollHeight) {
+  // Transition arrow opacities based on scroll height
+  function setArrowVisibility(scrollHeight) {
     let upArrow = document.getElementById("up-arrow");
     let downArrow = document.getElementById("down-arrow");
 
+    // If on title page, make only down arrow usable
     if (scrollHeight / WINDOW_HEIGHT_PIXELS < 1) {
+      upArrow.value = "disabled";
+      upArrow.style.cursor = "default";
       upArrow.style.opacity = 0;
+
+      downArrow.value = "regular";
+      downArrow.style.cursor = "pointer";
       downArrow.style.opacity = 1;
-    } else if (
+    }
+    // If in the middle, make both arrows usuable
+    else if (
       (document.documentElement.scrollHeight - scrollHeight) /
         WINDOW_HEIGHT_PIXELS <
       1
     ) {
+      upArrow.value = "regular";
+      upArrow.style.cursor = "pointer";
       upArrow.style.opacity = 1;
+
+      downArrow.value = "disabled";
+      downArrow.style.cursor = "default";
       downArrow.style.opacity = 0;
-    } else {
+    }
+    // If on last page, make only up arrow usable
+    else {
+      upArrow.value = "regular";
+      upArrow.style.cursor = "pointer";
       upArrow.style.opacity = 1;
+
+      downArrow.value = "regular";
+      downArrow.style.cursor = "pointer";
       downArrow.style.opacity = 1;
     }
   }
 
-  function makeRows() {
+  // Make Visual Grid
+  function makeGrid() {
+    // Make Row of blocks for visual
     function makeRow() {
-      let cells = [];
+      let row = [];
       for (let i = 0; i < NUM_COLS / CELL_WIDTH; i++) {
         let el = (
           <Cell key={i} index={i}>
             <img src={personIcon} alt="icon representing a person" />
           </Cell>
         );
-        cells.push(el);
+        row.push(el);
       }
-      return cells;
+      return row;
     }
-    let rows = [];
+
+    let grid = [];
     for (var i = 0; i < NUM_ROWS / CELL_HEIGHT; i++) {
       let row = makeRow();
-      rows.push(
+      grid.push(
         <Row key={i} index={i}>
           {row}
         </Row>
       );
     }
-    return rows;
+
+    return grid;
   }
 
-  /*****************************************************************************************/
+  /* Stuff that happens on page load ******************************************/
 
-  let rows = makeRows();
+  let grid = makeGrid();
   window.onscroll = onPageScroll;
+
+  /* HTML Rendering ***********************************************************/
 
   return (
     <div className="App">
+      {/* Title Page */}
       <header className="App-header">
         <div id="up-arrow" className="arrow up" onClick={onUpArrowClick} />
         <p>Visual Statistics</p>
@@ -131,85 +176,22 @@ function App() {
           onClick={onDownArrowClick}
         />
       </header>
+      {/* Visual and Slides */}
       <div id="Tutorial-body" className="Tutorial-body">
         <Grid id="tutorial-visual" className="tutorial-visual">
-          {rows}
+          {grid}
         </Grid>
         <div className="Tutorial-section">
-          <div className="Tutorial-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
-            Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
-            neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
-            facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
-            Maecenas augue lectus, eleifend id orci non, ornare pellentesque
-            sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
-            vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
-            Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
-            est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
-            libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
-            eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
-            Pellentesque non quam et felis condimentum malesuada a id nisl.
-            Etiam finibus mi nunc. Nam lobortis in eros et auctor.
-          </div>
+          <div className="Tutorial-text">{SlideContent[1]}</div>
         </div>
         <div className="Tutorial-section">
-          <div className="Tutorial-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
-            Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
-            neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
-            facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
-            Maecenas augue lectus, eleifend id orci non, ornare pellentesque
-            sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
-            vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
-            Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
-            est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
-            libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
-            eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
-            Pellentesque non quam et felis condimentum malesuada a id nisl.
-            Etiam finibus mi nunc. Nam lobortis in eros et auctor.
-          </div>
+          <div className="Tutorial-text">{SlideContent[2]}</div>
         </div>
         <div className="Tutorial-section">
-          <div className="Tutorial-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
-            Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
-            neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
-            facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
-            Maecenas augue lectus, eleifend id orci non, ornare pellentesque
-            sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
-            vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
-            Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
-            est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
-            libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
-            eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
-            Pellentesque non quam et felis condimentum malesuada a id nisl.
-            Etiam finibus mi nunc. Nam lobortis in eros et auctor.
-          </div>
+          <div className="Tutorial-text">{SlideContent[3]}</div>
         </div>
         <div className="Tutorial-section">
-          <div className="Tutorial-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
-            Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
-            neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
-            facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
-            Maecenas augue lectus, eleifend id orci non, ornare pellentesque
-            sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
-            vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
-            Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
-            est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
-            libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
-            eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
-            Pellentesque non quam et felis condimentum malesuada a id nisl.
-            Etiam finibus mi nunc. Nam lobortis in eros et auctor.
-          </div>
+          <div className="Tutorial-text">{SlideContent[4]}</div>
         </div>
       </div>
     </div>
---
+++ src/SlideContent.js
@@ -0,0 +1,87 @@
+export const SlideContent = {
+  1: `
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+   mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
+   Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
+   neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
+   facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
+   Maecenas augue lectus, eleifend id orci non, ornare pellentesque
+   sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
+   vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
+   Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
+   est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
+   libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
+   eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
+   Pellentesque non quam et felis condimentum malesuada a id nisl.
+   Etiam finibus mi nunc. Nam lobortis in eros et auctor.
+`,
+  2: `
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+   mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
+   Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
+   neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
+   facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
+   Maecenas augue lectus, eleifend id orci non, ornare pellentesque
+   sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
+   vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
+   Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
+   est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
+   libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
+   eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
+   Pellentesque non quam et felis condimentum malesuada a id nisl.
+   Etiam finibus mi nunc. Nam lobortis in eros et auctor.
+`,
+  3: `
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+   mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
+   Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
+   neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
+   facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
+   Maecenas augue lectus, eleifend id orci non, ornare pellentesque
+   sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
+   vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
+   Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
+   est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
+   libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
+   eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
+   Pellentesque non quam et felis condimentum malesuada a id nisl.
+   Etiam finibus mi nunc. Nam lobortis in eros et auctor.
+`,
+  4: `
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+   mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
+   Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
+   neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
+   facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
+   Maecenas augue lectus, eleifend id orci non, ornare pellentesque
+   sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
+   vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
+   Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
+   est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
+   libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
+   eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
+   Pellentesque non quam et felis condimentum malesuada a id nisl.
+   Etiam finibus mi nunc. Nam lobortis in eros et auctor.
+`,
+  5: `
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+   mauris erat, vulputate tincidunt quam et, porttitor vestibulum elit.
+   Nullam vel nibh ante. Suspendisse id aliquet ligula, nec iaculis
+   neque. Quisque sed sagittis libero. Donec nec magna vestibulum enim
+   facilisis pulvinar. Curabitur sed ante ac metus posuere dictum.
+   Maecenas augue lectus, eleifend id orci non, ornare pellentesque
+   sapien. Cras dignissim dolor id metus bibendum fringilla. Curabitur
+   vitae odio lacus. Nunc sed sapien ut orci eleifend vulputate.
+   Curabitur finibus lobortis augue sed laoreet. Sed venenatis lorem
+   est. Vestibulum placerat magna vel tortor scelerisque, quis sodales
+   libero consectetur. Proin porttitor ut sem ac elementum. Morbi vel
+   eros odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   Vestibulum viverra feugiat urna, vel tempor ligula vulputate et.
+   Pellentesque non quam et felis condimentum malesuada a id nisl.
+   Etiam finibus mi nunc. Nam lobortis in eros et auctor.
+`,
+};
